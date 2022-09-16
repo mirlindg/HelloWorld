48cc6ff73c5427d0382b34c112421c4bbf6e9c5b
Mirlind Guraziu <mirlindguraziu@MacBook-Air-von-Mirlind.local>
2022-09-16 19:06:47 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    //main // psvm tab
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World!");
+        System.out.println("Welcome to KONF 2022");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4"); // STRG + D
+        // Roter Punkt wenn man auf der Seite links drückt
+ // Programm debuggen und dann wieder zurück auf Console -> Mit F8 oder Button unten den Code Zeile für Zeile ausführen.
+
+    }
+}
+

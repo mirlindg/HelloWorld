521bfb887a435eb7bda6cc2e46420267de01bb46
mirlindg <s51525@edu.campus02.at>
2022-09-16 19:27:53 +0200

    Zeile 5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         System.out.println("Zeile 4"); // STRG + D
         // Roter Punkt wenn man auf der Seite links drückt
  // Programm debuggen und dann wieder zurück auf Console -> Mit F8 oder Button unten den Code Zeile für Zeile ausführen.
-
+        System.out.println("Zeile 5");
     }
 }
 
